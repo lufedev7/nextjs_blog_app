99acf3d7a7a557ce6000abcb88a310cb5213705e
Lufedev <luferdev@gmail.com>
2024-12-08 12:52:31 -0500

    update ubuntu

--- pages/api/posts/[postId].js
+++ pages/api/posts/[postId].js
@@ -1,16 +1,14 @@
 import data from '../data'
 
-
 // api/posts/1
-export default function handler(req, res){
-    const { postId } = req.query;
-    const { Posts }  = data;
+export default function handler(req, res) {
+  const { postId } = req.query
+  const { Posts } = data
+  const perro = 'tobi'
+  if (postId) {
+    const post = Posts.find((value) => value.id == postId)
+    return res.status(200).json(post)
+  }
 
-    if(postId){
-        const post = Posts.find( value => value.id == postId)
-        return res.status(200).json(post)
-    }
-
-    return res.status(404).json({ error : "Post Not Found"})
-
+  return res.status(404).json({ error: 'Post Not Found' })
 }
